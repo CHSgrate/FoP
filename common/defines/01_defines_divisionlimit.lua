aaffcc0466475425caea1c851f180f8ce65aa95a
kirill way <kilibos@proton.me>
2022-12-14 16:33:43 +0100

    bambass final fix

--- common/defines/01_defines_divisionlimit.lua
+++ common/defines/01_defines_divisionlimit.lua
@@ -1,4 +1,4 @@
-NDefines.NMilitary.CORPS_COMMANDER_DIVISIONS_CAP = 8			-- how many divisions a corps commander is limited to. 0 = inf, < 0 = blocked
-NDefines.NMilitary.CORPS_COMMANDER_ARMIES_CAP = -1				-- how many armies a corps commander is limited to. 0 = inf, < 0 = blocked
+NDefines.NMilitary.CORPS_COMMANDER_DIVISIONS_CAP = 6			-- how many divisions a corps commander is limited to. 0 = inf, < 0 = blocked
+NDefines.NMilitary.CORPS_COMMANDER_ARMIES_CAP = 8			-- how many armies a corps commander is limited to. 0 = inf, < 0 = blocked
 NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = 0			-- how many divisions a field marshall is limited to. 0 = inf, < 0 = blocked
 NDefines.NMilitary.FIELD_MARSHAL_ARMIES_CAP = 3				-- how many armies a field marshall is limited to. 0 = inf, < 0 = blocked
